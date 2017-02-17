4db5bd6c76820983aeae0858503869072a208864
Zsár Tamás <tamas@0xcefaedfe.com>
2017-02-17 17:19:18 +0100

    Created a dynamic framework target for SegmentFirebase

---
+++ Example/Pods/SegmentFirebase/SegmentFirebase.h
@@ -0,0 +1,19 @@
+//
+//  SegmentFirebase.h
+//  SegmentFirebase
+//
+//  Created by Zsár Tamás on 2017. 02. 17..
+//
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SegmentFirebase.
+FOUNDATION_EXPORT double SegmentFirebaseVersionNumber;
+
+//! Project version string for SegmentFirebase.
+FOUNDATION_EXPORT const unsigned char SegmentFirebaseVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SegmentFirebase/PublicHeader.h>
+
+
